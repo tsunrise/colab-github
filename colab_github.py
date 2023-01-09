7453308c97ef5c66e8c7c9e3a437761e29a98ec9
Tom Shen <tom.shen.cn@gmail.com>
2023-01-09 14:51:48 -0800

    Update colab_github.py

--- colab_github.py
+++ colab_github.py
@@ -13,7 +13,7 @@ def github_auth(persistant_key: bool):
     from google.colab import drive
     drive.mount('/content/drive/')
     private_key_dir = "/content/drive/MyDrive/.colab-github"
-    os.system("mkdir -p $private_key_dir")
+    os.system(f"mkdir -p {private_key_dir}")
   else:
     private_key_dir = "~/.ssh"
 
@@ -22,15 +22,15 @@ def github_auth(persistant_key: bool):
   import os
   if not os.path.exists(os.path.expanduser(private_key_path)):
     fresh_key = True
-    os.system("ssh-keygen -t ed25519 -f $private_key_path -N ''")
+    os.system(f"ssh-keygen -t ed25519 -f {private_key_path} -N ''")
   else:
     fresh_key = False
 
   if persistant_key:
     os.system("rm -f ~/.ssh/id_ed25519")
     os.system("rm -f ~/.ssh/id_ed25519.pub")
-    os.system("cp -s $private_key_path ~/.ssh/id_ed25519")
-    os.system("cp -s $public_key_path ~/.ssh/id_ed25519.pub")
+    os.system(f"cp -s {private_key_path} ~/.ssh/id_ed25519")
+    os.system(f"cp -s {public_key_path} ~/.ssh/id_ed25519.pub")
 
   with open(os.path.expanduser(public_key_path), "r") as f:
     public_key = f.read()
